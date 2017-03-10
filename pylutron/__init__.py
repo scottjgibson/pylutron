f5549983dde5a168b7b0e260a3c96534dc60d367
Scott Gibson <scottgibson@gmail.com>
2017-03-09 21:00:14 -0500

    Pico remote support

--- pylutron/__init__.py
+++ pylutron/__init__.py
@@ -475,6 +475,7 @@ class _RequestHelper(object):
     return ev
 
   def notify(self):
+    import pudb; pudb.set_trace()
     with self.__lock:
       events = self.__events
       self.__events = []
@@ -691,7 +692,7 @@ class Keypad(LutronEntity):
     params = [int(x) for x in args[2:]]
     _LOGGER.debug("Updating %d(%s): c=%d a=%d params=%s" % (
         self._integration_id, self._name, component, action, params))
-    self._query_waiters.notify()
+    import pudb; pudb.set_trace()
     for button in self._buttons:
         if button._num == component:
             if (action == 3):
